d5b6e65054e43a41a85dc9f7dc93d19691ec6e0b
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-28 18:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,20 +9,17 @@ const person = {
 const team = players;
 
 // Create a copy of the players array using the slice method
-const team1 = players.slice();
+const team1 =[...players];
 
 // Create a copy of the person object using the spread operator
-const cap1 = { ...person };
+// const cap1 = { ...person};
+let cap1 = Object.assign({},person);
 
 // Test modifying the original arrays and objects to ensure copies are independent
-players.push("NewPlayer");
-person.name = "UpdatedName";
+// players.push("NewPlayer");
+// person.name = "UpdatedName";
 
-// console.log("Original players:", players);
-// console.log("Original person:", person);
-// console.log("team:", team);
-// console.log("team1:", team1);
-// console.log("cap1:", cap1);
+
 window.players = players;
 window.person = person;
 window.team = team;
